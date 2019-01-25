acc80e76fe0bfe55cf03c08ee161bf6697e0ad32
Kiran <kiranmca2015@gmail.com>
2019-01-25 23:57:29 +0530

    'first'

---
+++ CBV/basic_app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
+from django.urls import reverse_lazy
+from django.views.generic import (View,TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView)
+
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = "Basic Injection"
+        return context
+
+class SchoolListView(ListView):
+    model = models.School
+    context_object_name = 'schools'
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
+class SchoolCreateView(CreateView):
+    model = models.School
+    fields = ('name','principal','location')
+
+class SchoolUpdateView(UpdateView):
+    model = models.School
+    fields = ('name','principal')
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
